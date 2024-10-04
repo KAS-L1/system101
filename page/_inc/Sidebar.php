b3f84746756399936b3890fcc2779cd191cfa353
KAS-L1 <kasl.54370906@gmail.com>
2024-10-04 18:29:58 +0800

    onti na lang man

--- page/_inc/Sidebar.php
+++ page/_inc/Sidebar.php
@@ -1,40 +1,68 @@
-        <div id="layoutSidenav">
-            <div id="layoutSidenav_nav">
-                <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
-                    <div class="sb-sidenav-menu">
-                        <div class="nav">
-                            <div class="sb-sidenav-menu-heading">Core</div>
-                            <a class="nav-link" href="index.php">
-                                <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
-                                Dashboard
-                            </a>
-                            <div class="sb-sidenav-menu-heading">Modules</div>
-                            <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseLayouts" aria-expanded="false" aria-controls="collapseLayouts">
-                                <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
-                                Procurement
-                                <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
-                            </a>
-                            <div class="collapse" id="collapseLayouts" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
-                                <nav class="sb-sidenav-menu-nested nav">
-                                    <a class="nav-link" href="layout-static.html">Purchase Order</a>
-                                    <a class="nav-link" href="layout-sidenav-light.html">Suppliers</a>
-                                    <a class="nav-link" href="layout-sidenav-light.html">Reports</a>
-                                    <a class="nav-link" href="layout-sidenav-light.html">Purchase Requistion</a>
-                                    <a class="nav-link" href="layout-sidenav-light.html">Request for Quote</a>
-                                    <a class="nav-link" href="layout-sidenav-light.html">Contract Management</a>
-                                    <a class="nav-link" href="layout-sidenav-light.html">Invoice</a>
-                                    <a class="nav-link" href="layout-sidenav-light.html">Vendor Management</a>
-                                    <a class="nav-link" href="layout-sidenav-light.html">Budget Approval</a>
-                                </nav>
-                            </div>
-                        </div>
+<div id="layoutSidenav">
+    <div id="layoutSidenav_nav">
+        <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
+            <div class="sb-sidenav-menu">
+                <div class="nav">
+                    <div class="sb-sidenav-menu-heading">Core</div>
+                    <a class="nav-link" href="index.php">
+                        <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
+                        Dashboard
+                    </a>
+                    <div class="sb-sidenav-menu-heading">Modules</div>
+                    <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseProcurement" aria-expanded="false" aria-controls="collapseProcurement">
+                        <div class="sb-nav-link-icon"><i class="fas fa-shopping-cart"></i></div>
+                        Procurement
+                        <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+                    </a>
+                    <div class="collapse" id="collapseProcurement" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
+                        <nav class="sb-sidenav-menu-nested nav">
+                            <a class="nav-link" href="purchase_order.php">Purchase Order</a>
+                            <a class="nav-link" href="suppliers.php">Suppliers</a>
+                            <a class="nav-link" href="reports.php">Reports</a>
+                            <a class="nav-link" href="purchase_requisition.php">Purchase Requisition</a>
+                            <a class="nav-link" href="request_for_quote.php">Request for Quote</a>
+                            <a class="nav-link" href="contract_management.php">Contract Management</a>
+                            <a class="nav-link" href="invoice.php">Invoice</a>
+                            <a class="nav-link" href="vendor_management.php">Vendor Management</a>
+                            <a class="nav-link" href="budget_approval.php">Budget Approval</a>
+                        </nav>
                     </div>
-                    <div class="sb-sidenav-footer">
-                        <div class="small">Logged in as:</div>
-                        Start Bootstrap
+                    <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseAudit" aria-expanded="false" aria-controls="collapseAudit">
+                        <div class="sb-nav-link-icon"><i class="fas fa-file-alt"></i></div>
+                        Audit Management
+                        <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+                    </a>
+                    <div class="collapse" id="collapseAudit" aria-labelledby="headingTwo" data-bs-parent="#sidenavAccordion">
+                        <nav class="sb-sidenav-menu-nested nav">
+                            <a class="nav-link" href="layout-static.html">Audit Reports</a>
+                            <a class="nav-link" href="layout-sidenav-light.html">Audit Schedules</a>
+                            <a class="nav-link" href="layout-sidenav-light.html">Audit Findings</a>
+                            <a class="nav-link" href="layout-sidenav-light.html">Audit Recommendations</a>
+                            <a class="nav-link" href="layout-sidenav-light.html">Audit Implementation</a>
+                        </nav>
                     </div>
-                </nav>
+                    <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseDocument" aria-expanded="false" aria-controls="collapseDocument">
+                        <div class="sb-nav-link-icon"><i class="fas fa-file"></i></div>
+                        Document Tracking
+                        <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+                    </a>
+                    <div class="collapse" id="collapseDocument" aria-labelledby="headingThree" data-bs-parent="#sidenavAccordion">
+                        <nav class="sb-sidenav-menu-nested nav">
+                            <a class="nav-link" href="layout-static.html">Document Upload</a>
+                            <a class="nav-link" href="layout-sidenav-light.html">Document Approval</a>
+                            <a class="nav-link" href="layout-sidenav-light.html">Document Storage</a>
+                            <a class="nav-link" href="layout-sidenav-light.html">Document Retrieval</a>
+                            <a class="nav-link" href="layout-sidenav-light.html">Document Disposal</a>
+                        </nav>
+                    </div>
+                    <div class="sb-sidenav-menu-heading">APPS</div>
+                </div>
             </div>
-            <div id="layoutSidenav_content">
-                
-         
+            <div class="sb-sidenav-footer">
+                <div class="small">Logged in as:</div>
+                Start Bootstrap
+            </div>
+        </nav>
+    </div>
+    <div id="layoutSidenav_content">
+        <!-- Your content here -->
