c5b354c432631f213159901b669635caa6b8b062
KAS-L1 <kasl.54370906@gmail.com>
2024-10-16 23:21:57 +0800

    get and post api end point

--- api/sync/procurement.php
+++ api/sync/procurement.php
@@ -1,7 +1,7 @@
 <?php
 require("../../app/init.php");
 
-$token = "d368051b3cd2819131fff6811cf4e59c"; //  Kupal ka ba boss?
+$token = "d368051b3cd2819131fff6811cf4e59c........"; //  Kupal ka ba boss?
 
 if (!isset($_GET['token']) or $_GET['token'] != $token) {
     die("Invalid token failed to request to server.");
@@ -12,3 +12,7 @@ $requisitions = $DB->SELECT("purchaserequisition", "*", "ORDER BY requisition_id
 header('Content-Type: application/json; charset=utf-8');
 
 echo json_encode($requisitions);
+
+// For 
+// $data = file_get_contents('https://logistic2.paradisehoteltomasmorato.com/api/sync/procurement.php?token=d368051b3cd2819131fff6811cf4e59c');
+// $data = json_decode($data, true);
