4fb3bedf353173904e169bb7d05053d085ad0cb3
KAS-L1 <kasl.54370906@gmail.com>
2024-11-08 20:01:21 +0800

    fix users

--- api/user/create.php
+++ api/user/create.php
@@ -6,10 +6,11 @@ use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
 // Escape and sanitize the input data
-$password = $_POST['username'] . rand(1000, 9999);
+$username = $DB->ESCAPE($_POST['username']);
+$password = $username . rand(1000, 9999);
 $data = array(
     "user_id" => rand(100000, 999999),
-    "username" => $DB->ESCAPE($_POST['username']),
+    "username" => $username,
     "password" => md5($password),
     "firstname" => $DB->ESCAPE($_POST['user_firstname']),
     "lastname" => $DB->ESCAPE($_POST['user_lastname']),
@@ -39,21 +40,23 @@ $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
 $mail->Port = 587;
 
 $email = $_POST['email'];
-$link = '<a href="http://' . $_SERVER['HTTP_HOST'] . '/login">http://' . $_SERVER['HTTP_HOST'] . '/login</a>';
+$link = '<a href="https://' . $_SERVER['HTTP_HOST'] . '/login">https://' . $_SERVER['HTTP_HOST'] . '/login</a>';
 
 // Sender and recipient
 $mail->setFrom('kasl.54370906@gmail.com', 'Logistic Paradise');
 $mail->addAddress($email);
+
 // Email Content
 $mail->isHTML(true);
 $mail->Subject = "Your Account Password";
 $mail->Body = "
+            Username: $username<br>
             Password: $password<br>
             Link: $link
             <br><br>
             Access your account via your password above kupal.
         ";
-$mail->AltBody = "Password access";
+$mail->AltBody = "Username: $username, Password: $password. Access your account at $link.";
 
 if ($mail->send()) {
     swalAlert("success", "User Added", "Password sent to email " . $email);
